19999674b83b5835aeed1829ba4137272e5799b8
hunterhhh412 <amanagnihotri412002@gmail.com>
2024-10-09 16:48:26 +0530

    Initial project setup for The Forge.

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(helmet());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("The Forge API is running.")
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`The Forge API is running on port ${port}.`);
+});
